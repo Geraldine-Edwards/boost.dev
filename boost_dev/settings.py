db75313a627973df747d9eda55323c4a43e1481e
Geraldine-Edwards <geraldine.edwards25@gmail.com>
2026-08-11 15:04:13 +0100

    Fix: Update CSRF_TRUSTED_ORIGINS for local development

--- boost_dev/settings.py
+++ boost_dev/settings.py
@@ -47,10 +47,10 @@ ALLOWED_HOSTS += ['ge-boost-dev.hosting.codeyourfuture.io']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CSRF_TRUSTED_ORIGINS = [
-    http://localhost:8000,
-    http://127.0.0.1:8000,
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
     'https://ge-boost-dev.hosting.codeyourfuture.io',
-]
+    ]
 
 
 # Application definition
